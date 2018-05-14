84e336813ce75e4308beb351c53952f2a8a4c63a
John David Ibañez <JhonhRge@gmail.com>
2018-05-14 14:40:27 -0500

    Create FW.py

---
+++ graphs/Python/FW.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from heapq import *
+
+def FW(src):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                 g[i][j] = min(g[i][j],(g[i][k]+g[k][j]));
+
+def main():
+    global g,Edges
+    n,e  = [int(x) for x in stdin.readline().strip().split()]
+    g = [ [ 0 for i in range(n+1)] for j in range(n)]
+    d = [ [ float('inf') for i in range(n+1)] for j in range(n)]
+    Edges = list()
+    for G in range(e):
+        u,v,w = [int(x) for x in stdin.readline().strip().split()]
+        g[u][v] = w
+        g[v][u] = w
+    print(dijkstra2(0))
+main()
+
